bbb94b357c1b2b00ec46e0f445c838584e46937a
Sangwoo Byeon <tkddn0518@naver.com>
2024-04-29 22:35:12 +0900

    [Setting] 프로젝트 생성 및 gitignore 파일 추가

---
+++ RestSharer/RestSharerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestSharerApp.swift
+//  RestSharer
+//
+//  Created by 변상우 on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct RestSharerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
