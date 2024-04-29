16de9145b493ed8ec79c9fc8b446fd44d7d2c0c9
Sangwoo Byeon <tkddn0518@naver.com>
2024-04-29 22:53:00 +0900

    [Setting] 폰트 추가 및 프로젝트 파일 구조 변경

---
+++ RestSharer/Extension/CGFloat+.swift
@@ -0,0 +1,13 @@
+//
+//  CGFloat+.swift
+//  RestSharer
+//
+//  Created by 변상우 on 4/29/24.
+//
+
+import SwiftUI
+
+extension CGFloat {
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
+}
