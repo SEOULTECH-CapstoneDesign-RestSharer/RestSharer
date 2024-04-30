14ff65a63721557fe944efe62a318030172dbada
Sangwoo Byeon <tkddn0518@naver.com>
2024-04-30 10:53:31 +0900

    [Feat] 파베 구글 로그인 기능 구현

---
+++ RestSharer/Store/AuthStore.swift
@@ -0,0 +1,132 @@
+//
+//  AuthStore.swift
+//  RestSharer
+//
+//  Created by 변상우 on 4/30/24.
+//
+
+import Foundation
+
+import SwiftUI
+import Firebase
+import FirebaseFirestore
+import FirebaseAuth
+import GoogleSignIn
+import Combine
+import AuthenticationServices
+import CryptoKit
+import FirebaseCore
+
+enum LoginPlatform {
+    case email
+    case google
+    case none
+}
+
+class AuthStore: ObservableObject {
+    @Published var currentUser: Firebase.User?
+    @Published var welcomeToast: Bool = false
+    @Published var loginPlatform: LoginPlatform = .google
+    
+    init() {
+        currentUser = Auth.auth().currentUser
+    }
+    
+    func signInGoogle() {
+        if GIDSignIn.sharedInstance.hasPreviousSignIn() {
+            GIDSignIn.sharedInstance.restorePreviousSignIn { [unowned self] user, error in
+                authenticateUser(for: user, with: error)
+            }
+        } else {
+            guard let cliendID = FirebaseApp.app()?.options.clientID else { return }
+            
+            let configuration = GIDConfiguration(clientID: cliendID)
+            
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+                return
+            }
+            
+            guard let rootViewController = windowScene.windows.first?.rootViewController else {
+                return
+            }
+            
+            GIDSignIn.sharedInstance.configuration = configuration
+            GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController) { [unowned self] result, error in
+                authenticateUser(for: result?.user, with: error)
+            }
+        }
+    }
+    
+    private func authenticateUser(for user: GIDGoogleUser?, with error: Error?) {
+        if let googleUser = user {
+            let email = googleUser.profile?.email ?? ""
+            let name = googleUser.profile?.name ?? ""
+            
+            let userData = User(email: email, name: name)
+            
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let idToken = user?.idToken?.tokenString, let accessToken = user?.accessToken.tokenString else {
+                return
+            }
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: accessToken)
+            
+            userCollection.whereField("email", isEqualTo: (user?.profile?.email)!).getDocuments { snapshot, error in
+                if snapshot!.documents.isEmpty {
+                    if let error = error {
+                        print(error.localizedDescription)
+                    } else {
+                        Auth.auth().signIn(with: credential) { [unowned self] (result, error) in
+                            if let error = error {
+                                print(error.localizedDescription)
+                            } else {
+                                self.currentUser = result?.user
+                                self.welcomeToast = true
+                                self.createUser(user: userData)
+                                self.loginPlatform = .google
+                            }
+                        }
+                    }
+                } else {
+                    userCollection.document((user?.profile?.email)!).getDocument { snapshot, error in
+                        _ = snapshot!.data()
+                        
+                        Auth.auth().signIn(with: credential) { result, error in
+                            if let error = error {
+                                print(error.localizedDescription)
+                                return
+                            } else {
+                                self.currentUser = result?.user
+                                self.welcomeToast = true
+                                self.loginPlatform = .google
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func signOutGoogle() {
+        GIDSignIn.sharedInstance.signOut()
+        
+        do {
+            try Auth.auth().signOut()
+            currentUser = nil
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func createUser(user: User) {
+        userCollection
+            .document(user.email)
+            .setData(["email" : user.email,
+                      "name" : user.name]
+            )
+    }
+}
