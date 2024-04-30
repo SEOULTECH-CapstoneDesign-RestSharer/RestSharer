59d1c8b0c0703049065428490cbcd27d2f653c22
Sangwoo Byeon <tkddn0518@naver.com>
2024-04-30 10:53:57 +0900

    [Feat] 파베 구글 로그인, 로그아웃 뷰 UI 구현 및 적용

---
+++ RestSharer/View/LaunchView.swift
@@ -0,0 +1,63 @@
+//
+//  LaunchView.swift
+//  RestSharer
+//
+//  Created by 변상우 on 4/30/24.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    
+    @EnvironmentObject var authStore: AuthStore
+    
+    @State private var isActive = false
+    @State private var isloading = true
+    
+    var body: some View {
+        if isActive {
+            if authStore.currentUser != nil {
+                ContentView()
+             } else {
+                 LoginView()
+             }
+        } else {
+            if isloading {
+                ZStack {
+                    Text("RestSharer")
+                }
+                .edgesIgnoringSafeArea(.all)
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                        withAnimation {
+                            self.isActive = true
+                            self.isloading.toggle()
+                        }
+                    }
+                    
+//                    if let email = authStore.currentUser?.email {
+//                        userStore.fetchCurrentUser(userEmail: email)
+//                        
+//                        userStore.fetchMyInfo(userEmail: email, completion: { result in
+//                            if result {
+//                                self.isActive = true
+//                                self.isloading.toggle()
+//                            }
+//                        })
+//                    } else {
+//                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+//                            withAnimation {
+//                                self.isActive = true
+//                                self.isloading.toggle()
+//                            }
+//                        }
+//                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    LaunchView()
+}
