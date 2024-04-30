d8e21dc7033008af8948e6a84ec0daec13903085
Sangwoo Byeon <tkddn0518@naver.com>
2024-04-30 10:53:13 +0900

    [Feat] 파베 경로 파일, 번들 파일 추가

---
+++ RestSharer/Commons/Path.swift
@@ -0,0 +1,12 @@
+//
+//  Path.swift
+//  RestSharer
+//
+//  Created by 변상우 on 4/30/24.
+//
+
+import Firebase
+
+let userCollection = Firestore.firestore().collection("User")
+let feedCollection = Firestore.firestore().collection("Feed")
+let shopCollection = Firestore.firestore().collection("Shop")
---
+++ RestSharer/Extension/Bundle+.swift
@@ -0,0 +1,8 @@
+//
+//  Bundle+.swift
+//  RestSharer
+//
+//  Created by 변상우 on 4/30/24.
+//
+
+import Foundation
