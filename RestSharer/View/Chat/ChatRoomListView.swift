0fb6774ddba89a9fdbf743414c01b53ba6ef4b6a
Sangwoo Byeon <tkddn0518@naver.com>
2024-09-30 00:44:20 +0900

    [Remove] Sendbird 채팅 라이브러리 사용 코드 삭제

--- RestSharer/View/Chat/ChatRoomListView.swift
+++ RestSharer/View/Chat/ChatRoomListView.swift
@@ -6,20 +6,8 @@
 //
 
 import SwiftUI
-import SendBirdUIKit
 
 struct ChatRoomListView: View {
-    init() {
-        // 1. Initialize Sendbird UIKit
-        SBUMain.initialize(applicationId:"D1E6401F-DB5B-4702-A3FB-E5F46D78D2D2")
-        // 2. Set the current user
-        SBUGlobals.CurrentUser = SBUUser(userId: "tkddn123987")
-        // 3. Connect to Sendbird
-        SBUMain.connect { (user, error) in
-            // user object will be an instance of SBDUser
-            guard let _ = user else {
-                print("ContentView: init: Sendbird connect: ERROR: \(String(describing: error)). Check applicationId")
-                return
             }
         }
     }
