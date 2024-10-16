927427e6ae8ba14314005dbca71391a154be36f3
Sangwoo Byeon <tkddn0518@naver.com>
2024-10-16 09:46:10 +0900

    [Feat] 애플 로그인 로직 함수 구현 완료

--- RestSharer/Store/AuthStore.swift
+++ RestSharer/Store/AuthStore.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-
 import SwiftUI
 import Firebase
 import FirebaseFirestore
@@ -20,20 +19,22 @@ import FirebaseCore
 enum LoginPlatform {
     case email
     case google
+    case apple
     case none
 }
 
-class AuthStore: ObservableObject {
+class AuthStore: NSObject, ObservableObject {
     @Published var currentUser: Firebase.User?
     @Published var welcomeToast: Bool = false
     @Published var loginPlatform: LoginPlatform = .google
     
     let userStore: UserStore = UserStore()
     
-    init() {
+    override init() {
         currentUser = Auth.auth().currentUser
     }
     
+    // 구글 로그인 함수
     func signInGoogle() {
         if GIDSignIn.sharedInstance.hasPreviousSignIn() {
             GIDSignIn.sharedInstance.restorePreviousSignIn { [unowned self] user, error in
@@ -59,6 +60,7 @@ class AuthStore: ObservableObject {
         }
     }
     
+    // 구글 로그인 후 사용자 정보 처리
     private func authenticateUser(for user: GIDGoogleUser?, with error: Error?) {
         if let googleUser = user {
             let email = googleUser.profile?.email ?? ""
@@ -127,17 +129,120 @@ class AuthStore: ObservableObject {
         }
     }
     
-    func signOutGoogle() {
-        GIDSignIn.sharedInstance.signOut()
+    // 애플 로그인
+    func signInWithApple() {
+        print("============signInWithApple============")
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.fullName, .email]
+
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    // 애플 로그인 후 사용자 정보 처리
+    func handleAuthorization(_ authorization: ASAuthorization) {
+        print("============handleAuthorization============")
+        guard let credential = authorization.credential as? ASAuthorizationAppleIDCredential else { return }
         
+        if let fullName = credential.fullName,
+           let identifyToken = credential.identityToken {
+            let userName = (fullName.familyName ?? "") + (fullName.givenName ?? "")
+            let email = credential.email
+            
+            guard let identityToken = credential.identityToken else {
+                print("애플 로그인 토큰을 가져올 수 없습니다.")
+                return
+            }
+
+            guard let idTokenString = String(data: identityToken, encoding: .utf8) else {
+                print("토큰 문자열 변환 실패")
+                return
+            }
+            
+            let firebaseCredential = OAuthProvider.credential(
+                withProviderID: "apple.com",
+                idToken: idTokenString,
+                rawNonce: "" // Nonce는 선택적입니다.
+            )
+            
+            
+            Auth.auth().signIn(with: firebaseCredential) { (authResult, error) in
+                if let error = error {
+                    print("Firebase 애플 로그인 실패: \(error.localizedDescription)")
+                    return
+                }
+                
+                userCollection.whereField("email", isEqualTo: (authResult?.user.email)!).getDocuments { snapshot, error in
+                    if snapshot!.documents.isEmpty {
+                        if let error = error {
+                            print(error.localizedDescription)
+                        } else {
+                            let userData: [String: Any] = ["email" : authResult?.user.email ?? "",
+                                                           "name" : "appleLogin",
+                                                           "nickname" : "",
+                                                           "phoneNumber" : "",
+                                                           "profileImageURL" : "",
+                                                           "follower" : [],
+                                                           "following" : [],
+                                                           "myFeed" : [],
+                                                           "savedFeed" : [],
+                                                           "bookmark" : [],
+                                                           "chattingRoom" : [],
+                                                           "myReservation" : []
+                            ]
+                            
+                            if let user = User(document: userData) {
+                                self.currentUser = authResult?.user
+                                self.welcomeToast = true
+                                self.loginPlatform = .apple
+                                
+                                self.userStore.createUser(user: user)
+                            }
+                        }
+                    } else {
+                        userCollection.document((authResult?.user.email)!).getDocument { snapshot, error in
+                            _ = snapshot!.data()
+                            
+                            self.currentUser = authResult?.user
+                            self.welcomeToast = true
+                            self.loginPlatform = .apple
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func doubleCheckNickname(nickname: String) async -> Bool {
+        do {
+            let datas = try await userCollection.whereField("nickname", isEqualTo: nickname).getDocuments()
+            if !datas.isEmpty {
+                return false
+            } else {
+                return true
+            }
+        }
+        catch {
+            debugPrint("getDocument 에러")
+            return false
+        }
+    }
+    
+    // 공통 로그아웃 함수
+    func signOut() {
         do {
             try Auth.auth().signOut()
             currentUser = nil
+            loginPlatform = .none
         } catch {
             print(error.localizedDescription)
         }
     }
-    func deleteAuth (_ email: String) {
+    
+    // 사용자 계정 삭제
+    func deleteAuth() {
         Auth.auth().currentUser?.delete { error in
             if let error = error {
                 print("사용자 삭제 중 오류 발생: \(error.localizedDescription)")
@@ -145,6 +250,24 @@ class AuthStore: ObservableObject {
                 print("사용자 삭제 완료")
             }
         }
-        
+    }
+}
+
+// MARK: - ASAuthorizationControllerDelegate
+
+extension AuthStore: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        handleAuthorization(authorization)
+    }
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("애플 로그인 오류: \(error.localizedDescription)")
+    }
+
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+            return windowScene.windows.first { $0.isKeyWindow } ?? windowScene.windows[0]
+        }
+        fatalError("No key window available")
     }
 }
